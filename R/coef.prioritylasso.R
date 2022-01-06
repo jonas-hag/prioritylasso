9e570f72800cbc85bb778a3fff9762958658554b
Jonas Hagenberg <38041374+jonas-hag@users.noreply.github.com>
2022-01-06 17:56:06 +0100

    adjust coef function to correct format

--- R/coef.prioritylasso.R
+++ R/coef.prioritylasso.R
@@ -1,38 +1,39 @@
 #' Extract coefficients from a prioritylasso object
 #'
-#' @param model 
+#' @param object model of type prioritylasso
+#' @param ... additional arguments, currently not used
 #'
 #' @return List with the coefficients and the intercepts
 #' @export
-coef.prioritylasso <- function(model) {
-  if (!"prioritylasso" %in% class(model)) {
+coef.prioritylasso <- function(object, ...) {
+  if (!"prioritylasso" %in% class(object)) {
     stop("The model has to be of class prioritylasso.")
   }
   
   # get the coefficients
-  if (model$call$family == "cox") {
-    coefficients <- model$coefficients
+  if (object$call$family == "cox") {
+    coefficients <- object$coefficients
   } else {
-    if (is.null(model$block1unpen)) {
-      coefficients <- model$coefficients
+    if (is.null(object$block1unpen)) {
+      coefficients <- object$coefficients
     } else {
-      coefficients <- model$coefficients[-1]
+      coefficients <- object$coefficients[-1]
     }
   }
   
   # get the intercepts
-  if (model$call$family == "cox") {
+  if (object$call$family == "cox") {
     intercepts <- NULL
   } else {
-    if (is.null(model$block1unpen)) {
+    if (is.null(object$block1unpen)) {
       intercepts <- c()
-      for (i in 1:length(model$glmnet.fit)) {
-        intercepts[i] <- model$glmnet.fit[[i]]$a0[model$lambda.ind[[i]]]
+      for (i in 1:length(object$glmnet.fit)) {
+        intercepts[i] <- object$glmnet.fit[[i]]$a0[object$lambda.ind[[i]]]
       }
     } else {
-      intercepts <- model$coefficients[1]
-      for (i in 2:length(model$glmnet.fit)) {
-        intercepts[i] <- model$glmnet.fit[[i]]$a0[model$lambda.ind[[i]]]
+      intercepts <- object$coefficients[1]
+      for (i in 2:length(object$glmnet.fit)) {
+        intercepts[i] <- object$glmnet.fit[[i]]$a0[object$lambda.ind[[i]]]
       }
     }
   }
